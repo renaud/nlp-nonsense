743d5811bfc23e396b02d09632badf50c1fe06bb
Ian Tenney <iftenney@stanford.edu>
2014-11-02 15:26:02 -0800

    Added a (really) simple pre-filter script
    For now, just removes tiny fragments, single words, and anything containing the word "porn".

---
+++ prefilter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import sys, os, re
+
+def custom_filter(line):
+    # Remove really short things (likely fragments)
+    if len(line) < 8: return False
+
+    # Remove single words
+    if len(line.split()) < 2: return False
+
+    # Remove obvious porn
+    ll = line.lower()
+    if re.search("porn", ll): return False
+
+    return True
+
+##
+# Really simple line filtering
+for line in sys.stdin:
+    line = line.strip()
+    if custom_filter(line):
+        print line
