0c7b0fcf4d4047ed4448e455c2e30d7a92762a92
Ian Tenney <iftenney@stanford.edu>
2014-11-12 21:14:55 -0800

    Set up Java sources and IntelliJ project

---
+++ src/edu/stanford/iftenney/Main.java
@@ -0,0 +1,19 @@
+package edu.stanford.iftenney;
+
+import java.io.*;
+import java.util.*;
+import edu.stanford.nlp.io.*;
+import edu.stanford.nlp.ling.*;
+import edu.stanford.nlp.pipeline.*;
+import edu.stanford.nlp.trees.*;
+import edu.stanford.nlp.trees.TreeCoreAnnotations.*;
+import edu.stanford.nlp.util.*;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Hello World!");
+        System.out.println("If you see this, I got the build system to work.");
+    }
+}
---
+++ src/edu/stanford/iftenney/StanfordCoreNlpExample.java
@@ -0,0 +1,37 @@
+package edu.stanford.iftenney;
+
+import java.io.*;
+import java.util.*;
+import edu.stanford.nlp.io.*;
+import edu.stanford.nlp.ling.*;
+import edu.stanford.nlp.pipeline.*;
+import edu.stanford.nlp.trees.*;
+import edu.stanford.nlp.trees.TreeCoreAnnotations.*;
+import edu.stanford.nlp.util.*;
+
+public class StanfordCoreNlpExample {
+    public static void main(String[] args) throws IOException {
+        PrintWriter xmlOut = new PrintWriter("xmlOutput.xml");
+        Properties props = new Properties();
+        props.setProperty("annotators",
+                "tokenize, ssplit, pos, lemma, ner, parse");
+        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+        Annotation annotation = new Annotation(
+                "This is a short sentence. And this is another.");
+        pipeline.annotate(annotation);
+        pipeline.xmlPrint(annotation, xmlOut);
+
+        // An Annotation is a Map and you can get and use the
+        // various analyses individually. For instance, this
+        // gets the parse tree of the 1st sentence in the text.
+        List<CoreMap> sentences = annotation.get(
+                CoreAnnotations.SentencesAnnotation.class);
+        if (sentences != null && sentences.size() > 0) {
+            CoreMap sentence = sentences.get(0);
+            Tree tree = sentence.get(TreeAnnotation.class);
+            PrintWriter out = new PrintWriter(System.out);
+            out.println("The first sentence parsed is:");
+            tree.pennPrint(out);
+        }
+    }
+}
